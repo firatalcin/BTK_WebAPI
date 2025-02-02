645000655435ffc621a6c7e7df1c61d2aaf712f9
Fırat Alçın <firatalcin04@gmail.com>
2025-02-02 17:42:38 +0300

    EntityFramework Core

---
+++ bsStoreApp/WebApi/Controllers/BooksController.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context.Books.Find(id);
+                
+                if(book is null)
+                    return NotFound();
+                
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(book.Title))
+                    return BadRequest();
+                
+                _context.Books.Add(book);   
+                _context.SaveChanges();
+                return StatusCode(201, book);   
+    
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,[FromBody] Book book)
+        {
+            try
+            {
+                var enttiy = _context.Books.Where(x => x.Id == id).SingleOrDefault();
+                
+                if(enttiy is null)
+                    return NotFound();
+                
+                if(id != book.Id)
+                    return BadRequest();    
+                
+                enttiy.Title = book.Title;
+                enttiy.Price = book.Price;
+                _context.SaveChanges();
+                return Ok(book);
+                
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(x => x.Id == id).SingleOrDefault();
+
+                if (entity is null)
+                {
+                    return NotFound(new
+                    {
+                        statusCode = 404,
+                        message = $"Book with id: {id} not found"
+                    });
+                }
+                
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+                return Ok(entity);
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
