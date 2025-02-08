fc0c0d068add0a9182abafa9566597fa7c4b7588
Fırat Alçın <firatalcin04@gmail.com>
2025-02-08 13:41:39 +0300

    RepositoryManager

---
+++ bsStoreApp/Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,20 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _context;
+
+    public RepositoryManager(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public IBookRepository Book => new BookRepository(_context);
+    
+    public void Save()
+    {
+        _context.SaveChanges();
+    }
+}
