987eee0bd69a28d54f8e648c6d5472bfc1aa8194
Fırat Alçın <firatalcin04@gmail.com>
2025-02-08 13:30:17 +0300

    BookRepository

---
+++ bsStoreApp/Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+
+public class BookRepository : RepositoryBase<Book>, IBookRepository
+{
+    public BookRepository(RepositoryContext context) : base(context)
+    {
+    }
+
+
+    public IQueryable<Book> GetAllBooks(bool trackChanges)
+    {
+        return FindAll(trackChanges)
+            .OrderBy(b => b.Id);
+    }
+
+    public IQueryable<Book> GetOneBookById(int id, bool trackChanges)
+    {
+        return FindByCondition(b => b.Id == id, trackChanges);
+    }
+
+    public void CreateOneBook(Book book)
+    {
+        Create(book);
+    }
+
+    public void UpdateOneBook(Book book)
+    {
+        Update(book);
+    }
+
+    public void DeleteOneBook(Book book)
+    {
+        Delete(book);
+    }
+}
