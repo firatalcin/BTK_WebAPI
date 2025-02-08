01297901330dd62907b37475d19f44e840961ec4
Fırat Alçın <firatalcin04@gmail.com>
2025-02-08 18:31:48 +0300

    IBookService

---
+++ bsStoreApp/Services/Managers/BookManager.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services.Managers;
+
+public class BookManager : IBookService
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public BookManager(IRepositoryManager repositoryManager)
+    {
+        _repositoryManager = repositoryManager;
+    }
+
+    public IEnumerable<Book> GetAllBooks(bool trackChanges)
+    {
+        return _repositoryManager.Book.GetAllBooks(trackChanges);
+    }
+
+    public Book GetOneBookById(int id, bool trackChanges)
+    {
+        return _repositoryManager.Book.GetOneBookById(id, trackChanges);
+    }
+
+    public Book CreateOneBook(Book book)
+    {
+        if(book is null)
+            throw new ArgumentNullException(nameof(book));
+        
+        _repositoryManager.Book.CreateOneBook(book);
+        _repositoryManager.Save();
+        return book;
+    }
+
+    public void UpdateOneBook(int id, Book book, bool trackChanges)
+    {
+        var entity = _repositoryManager.Book.GetOneBookById(id, trackChanges);
+        
+        if(entity is null)
+            throw new Exception($"Book with id: {id} could not found");
+        
+        if(book is null)
+            throw new ArgumentNullException(nameof(book));
+        
+        entity.Title = book.Title;
+        entity.Price = book.Price;
+        
+        _repositoryManager.Book.UpdateOneBook(entity);
+        _repositoryManager.Save();
+
+    }
+
+    public void DeleteOneBook(int id, bool trackChanges)
+    {
+        var entity = _repositoryManager.Book.GetOneBookById(id, trackChanges);
+        
+        if(entity is null)
+            throw new Exception($"Book with id: {id} could not found");
+        
+        _repositoryManager.Book.DeleteOneBook(entity);
+        _repositoryManager.Save();
+    }
+}
