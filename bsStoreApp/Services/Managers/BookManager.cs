0c9c7c8670180eba3c17f5e29f01d94f6988407b
Fırat Alçın <firatalcin04@gmail.com>
2025-02-10 23:11:45 +0300

    NLog Mimaride Kullanımı

--- bsStoreApp/Services/Managers/BookManager.cs
+++ bsStoreApp/Services/Managers/BookManager.cs
@@ -7,10 +7,12 @@ namespace Services.Managers;
 public class BookManager : IBookService
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly ILoggerService _logger;
 
-    public BookManager(IRepositoryManager repositoryManager)
+    public BookManager(IRepositoryManager repositoryManager, ILoggerService logger)
     {
         _repositoryManager = repositoryManager;
+        _logger = logger;
     }
 
     public IEnumerable<Book> GetAllBooks(bool trackChanges)
@@ -25,8 +27,10 @@ public class BookManager : IBookService
 
     public Book CreateOneBook(Book book)
     {
-        if(book is null)
-            throw new ArgumentNullException(nameof(book));
+        if (book.Title.Length < 2)
+        {
+            _logger.LogInfo("Book title is more than 2 characters.");
+        }
         
         _repositoryManager.Book.CreateOneBook(book);
         _repositoryManager.Save();
@@ -36,9 +40,15 @@ public class BookManager : IBookService
     public void UpdateOneBook(int id, Book book, bool trackChanges)
     {
         var entity = _repositoryManager.Book.GetOneBookById(id, trackChanges);
-        
-        if(entity is null)
-            throw new Exception($"Book with id: {id} could not found");
+
+        if (entity is null)
+        {
+            var message = $"Book with id: {id} could not found";
+            _logger.LogInfo(message);
+            throw new Exception(message);
+        }
+            
+
         
         if(book is null)
             throw new ArgumentNullException(nameof(book));
@@ -54,9 +64,12 @@ public class BookManager : IBookService
     public void DeleteOneBook(int id, bool trackChanges)
     {
         var entity = _repositoryManager.Book.GetOneBookById(id, trackChanges);
-        
-        if(entity is null)
+
+        if (entity is null)
+        {
+            _logger.LogInfo($"Book with id: {id} could not found");
             throw new Exception($"Book with id: {id} could not found");
+        }
         
         _repositoryManager.Book.DeleteOneBook(entity);
         _repositoryManager.Save();
