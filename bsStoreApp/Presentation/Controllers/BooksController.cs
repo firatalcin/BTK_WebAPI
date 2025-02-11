4c27bb6f329d1329d17e7504757a6a7d53351cbe
Fırat Alçın <firatalcin04@gmail.com>
2025-02-11 23:19:47 +0300

    Try Catch Bloklarının Kaldırılması

--- bsStoreApp/Presentation/Controllers/BooksController.cs
+++ bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -7,95 +7,54 @@ namespace Presentation.Controllers;
 [ApiController]
 [Route("api/books")]
 public class BooksController : ControllerBase
+{
+    private readonly IServiceManager _manager;
+
+    public BooksController(IServiceManager manager)
     {
-        private readonly IServiceManager _manager;
-
-        public BooksController(IServiceManager manager)
-        {
-            _manager = manager; 
-        }
-
-        [HttpGet]
-        public IActionResult GetAllBooks()
-        {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
-        [HttpGet("{id:int}")]
-        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
-        {
-            try
-            {
-                var book = _manager.BookService.GetOneBookById(id, false);
-                
-                if(book is null)
-                    return NotFound();
-                
-                return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        [HttpPost]
-        public IActionResult CreateOneBook([FromBody] Book book)
-        {
-            try
-            {
-                if(string.IsNullOrEmpty(book.Title))
-                    return BadRequest();
-                
-                _manager.BookService.CreateOneBook(book);
-                return StatusCode(201, book);   
-    
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
-        [HttpPut("{id:int}")]
-        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,[FromBody] Book book)
-        {
-            try
-            {
-                if(book is null && id <= 0)
-                    return BadRequest();
-                
-                _manager.BookService.UpdateOneBook(id, book, true);
-               
-                return NoContent();
-                
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
-        [HttpDelete("{id:int}")]
-        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
-        {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, true);
-                return NoContent();
-            }
-            catch (Exception e)
-            {
-                
-                throw new Exception(e.Message);
-            }
-        }
+        _manager = manager;
     }
+
+    [HttpGet]
+    public IActionResult GetAllBooks()
+    {
+        var books = _manager.BookService.GetAllBooks(false);
+        return Ok(books);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+    {
+        var book = _manager.BookService.GetOneBookById(id, false);
+        if (book is null)
+            return NotFound();
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public IActionResult CreateOneBook([FromBody] Book book)
+    {
+        if (string.IsNullOrEmpty(book.Title))
+            return BadRequest();
+
+        _manager.BookService.CreateOneBook(book);
+        return StatusCode(201, book);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+    {
+        if (book is null && id <= 0)
+            return BadRequest();
+
+        _manager.BookService.UpdateOneBook(id, book, true);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+    {
+        _manager.BookService.DeleteOneBook(id, true);
+        return NoContent();
+    }
+}
