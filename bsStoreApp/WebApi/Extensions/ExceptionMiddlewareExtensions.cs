9a091f8bec32fb6579216cbbe1c0703efe80bfa4
Fırat Alçın <firatalcin04@gmail.com>
2025-02-16 18:56:22 +0300

    Use Exception Handler

--- bsStoreApp/WebApi/Extensions/ExceptionMiddlewareExtensions.cs
+++ bsStoreApp/WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Services.Contracts;
 
@@ -13,16 +14,22 @@ public static class ExceptionMiddlewareExtensions
         {
             appError.Run(async context =>
             {
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                
                 context.Response.ContentType = "application/json";
                 var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                 if (contextFeature is not null)
                 {
-                    logger.LogError($"Internal Server Error: {contextFeature.Error}");
+                    context.Response.StatusCode = contextFeature.Error switch
+                    {
+                        NotFound => StatusCodes.Status404NotFound,
+                        _ => StatusCodes.Status500InternalServerError,
+                    };
+                    
+                    logger.LogError($"Something went wrong: {contextFeature.Error}");
                     await context.Response.WriteAsync(new ErrorDetails()
                     {
                         StatusCode = context.Response.StatusCode,
-                        Message = "Internal Server Error",
+                        Message = contextFeature.Error.Message
                     }.ToString());
                 }
             });
