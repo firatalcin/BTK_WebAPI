315abbf713d0d0928bf3ffbc89babd6fe6397594
Fırat Alçın <firatalcin04@gmail.com>
2025-03-03 23:37:29 +0300

    Asenkron kod için sunum katmanının düzenlenmesi

--- bsStoreApp/Presentation/Controllers/BooksController.cs
+++ bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -18,21 +18,21 @@ public class BooksController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAllBooks()
+    public async Task<IActionResult> GetAllBooksAsync()
     {
-        var books = _manager.BookService.GetAllBooks(false);
+        var books = await _manager.BookService.GetAllBooksAsync(false);
         return Ok(books);
     }
 
     [HttpGet("{id:int}")]
-    public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+    public async Task<IActionResult> GetOneBookAsync([FromRoute(Name = "id")] int id)
     {
-        var book = _manager.BookService.GetOneBookById(id, false);
+        var book = await _manager.BookService.GetOneBookByIdAsync(id, false);
         return Ok(book);
     }
 
     [HttpPost]
-    public IActionResult CreateOneBook([FromBody] BookDtoForInsertion bookDto)
+    public async Task<IActionResult> CreateOneBookAsync([FromBody] BookDtoForInsertion bookDto)
     {
         if (string.IsNullOrEmpty(bookDto.Title))
             return BadRequest();
@@ -40,12 +40,12 @@ public class BooksController : ControllerBase
         if (!ModelState.IsValid)
             return UnprocessableEntity(ModelState);
 
-        var book = _manager.BookService.CreateOneBook(bookDto);
+        var book = await _manager.BookService.CreateOneBookAsync(bookDto);
         return StatusCode(201, book);
     }
 
     [HttpPut("{id:int}")]
-    public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] BookDtoForUpdate bookDto)
+    public async Task<IActionResult> UpdateOneBookAsync([FromRoute(Name = "id")] int id, [FromBody] BookDtoForUpdate bookDto)
     {
         if (bookDto is null && id <= 0)
             return BadRequest();
@@ -53,14 +53,14 @@ public class BooksController : ControllerBase
         if (!ModelState.IsValid)
             return UnprocessableEntity(ModelState);
 
-        _manager.BookService.UpdateOneBook(id, bookDto, false);
+        await _manager.BookService.UpdateOneBookAsync(id, bookDto, false);
         return NoContent();
     }
 
     [HttpDelete("{id:int}")]
-    public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+    public async Task<IActionResult> DeleteOneBookAsync([FromRoute(Name = "id")] int id)
     {
-        _manager.BookService.DeleteOneBook(id, true);
+        await _manager.BookService.DeleteOneBookAsync(id, true);
         return NoContent();
     }
 }
